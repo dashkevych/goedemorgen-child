3eb19f796c3e41dac05df62085393586609c41ef
Taras Dashkevych <trdashix@gmail.com>
2017-06-30 18:09:02 -0700

    Add: support for child theme language files;

--- functions.php
+++ functions.php
@@ -9,6 +9,16 @@
  * @since 1.0
  */
 
+ /**
+  *  Sets up child theme defaults and registers support for various WordPress features.
+  *
+  * @link http://codex.wordpress.org/Child_Themes
+  */
+ function goedemorgen_child_theme_setup() {
+     load_child_theme_textdomain( 'goedemorgen', get_stylesheet_directory() . '/languages' );
+ }
+ add_action( 'after_setup_theme', 'goedemorgen_child_theme_setup' );
+
 /**
  * Enqueue parent style.
  *
